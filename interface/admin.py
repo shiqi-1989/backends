6f9e791c3f3aec0644c852be7c3e32b08cfcce48
Administrator <386456612@qq.com>
2024-05-18 14:20:45 +0800

    优化环境配置

--- interface/admin.py
+++ interface/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from interface.models import *
 
+
 # Register your models here.
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'mobile', 'gender')
 
-admin.site.register(User)
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Api)
 admin.site.register(Case)
 admin.site.register(Report)
